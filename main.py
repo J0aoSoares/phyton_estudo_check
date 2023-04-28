83ec84a67d613dc5152cb862818180f9810be490
joao <joaorodrigues.009@outlook.com>
2023-04-28 20:59:58 -0300

    aula finalizada

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+'''
 resposta = input("Você quer conhecer minha calculadora ? (s/n): ")
 while resposta != "s" and resposta != "n":  #not (resposta == "s" or resposta == "n")
     resposta = input("Tem que ser s ou n: ")
@@ -42,4 +43,48 @@ else:
             continue
         else:
             break
+
+
+for i in range(10,-10,3): #(start, ent,step)
+    print(i)
+lista = [1, False, 2.0,"name"]
+print(lista)
+
+
+lista = [1, "maça", 2, "ad", 45, "asdda", 12, 67, 87, False]
+qtd_ints = 0
+for elem in lista:
+    if type(elem) is int:
+        qtd_ints+=1
+print(f"na lista há {qtd_ints} na lista")
+
+
+lista = [1, "maça", 2, "ad", 45, "asdda", 12, 67, 87, False]
+indice = 0
+for elem in lista:
+    if type(elem) is bool:
+        indice_bool = indice
+    indice+=1
+print(f"no ind {indice_bool} há uma var booleana")
+
+
+for i in range(len(lista)):
+    if type(lista[i]) is bool:
+        indice_bool = i
+print(f"no ind {indice_bool} há uma var booleana")
+
+'''
+
+lista = ["Danilo", "Well", "Pizzo", "Eduardo"]
+frase = ""
+for i in range(len(lista)):
+    if i == len(lista)-1:
+        frase += f"e o {lista[i]}"
+        continue
+    frase += f"o {lista[i]}"
+    if i<len(lista)-2:
+        frase+=", "
+    frase+= f"o {lista[i]}, "
+frase+= " são professores."
+print(frase)
 
